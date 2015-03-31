7964c0d9299c85b70a3c357499cba5e1b0b921fb
Bretton Auerbach <bretton.auerbach@gmail.com>
2015-03-30 20:18:50 -0700

    Add gameOverObject with won set to false for newGame. Update conditional to only allow moves when no one has won (in addition to existing restriction to blank cells. Change alert of winning message to fade in and out on the page. Add init() function to create a new game on page load (needed since board was moved into an anonymous function for use in newGame. Angular docs advise against ngInit for this purpose--see inline comments.).

--- gamePlayScript.js
+++ gamePlayScript.js
@@ -27,42 +27,47 @@
       };
 
       this.newGame = function(){
+        this.winner = {won: false};
         this.board = this.newBoard();
         this.arrayBoard = this.board.arrayBoard;
         this.integerBoard = this.board.integerBoard;
       }
 
-      this.gameWon = function(turn, intBoard){                         // Tests board for win values
-        if (turn === "X"){                                               // If it's x's turn
+      this.gameWon = function(turn, intBoard){                        // Tests board for win values
+        if (turn === "X"){                                            // If it's x's turn
           for (var i = 0; i < this.XWINS.length; i++){
-            if ((this.XWINS[i] | intBoard) === intBoard){                // Check if x win values are on the board with
+            if ((this.XWINS[i] | intBoard) === intBoard){             // Check if x win values are on the board with
               console.log("X won")
-              return{won: true, msg: "X has won."};                      // binary or and return true if value found
+              return{won: true, msg: "X HAS WON"};                    // binary or and return true if value found
             }
           }
-        } else{                                                          // If it's o's turn
+        } else{                                                       // If it's o's turn
         for (var i = 0; i < this.OWINS.length; i++){
-            if ((this.OWINS[i] | intBoard) === intBoard){                // Check if o win values are on the board with
+            if ((this.OWINS[i] | intBoard) === intBoard){             // Check if o win values are on the board with
               console.log("O won")
-              return{won: true, msg: "O has won."};                      // binary or and return true if value found
+              return{won: true, msg: "O HAS WON"};                    // binary or and return true if value found
             }
           }
         }
-        return false;                                                     // If no wins found, no one has won, return false
+        return {won: false};;                                         // If no wins found, no one has won, return false
       }
 
       this.makeMove = function(idx){
         row = (Math.floor(idx/3));
         column = idx%3;
         activeCell = this.arrayBoard[row][column];
-        if (activeCell.mark === ""){
+        if (activeCell.mark === "" && !this.winner.won){
           activeCell.mark = "X";
           power = activeCell.idx;
           this.integerBoard += Math.pow(2,power);
           console.log('integer board value should show updated value: ', this.integerBoard)
-          winner = this.gameWon("X", this.integerBoard);
-          if (winner.won === true){
-            alert(winner.msg);
+          this.winner = this.gameWon("X", this.integerBoard);
+          if (this.winner.won === true){
+            this.gameOverMessage = this.winner.msg;
+            $scope.showMessage = true;
+            $timeout(function(){                                      // Delay hiding of message for fade in and fade out to run
+              $scope.showMessage = false;
+            }, 2000);
           };
           //else
           //computer make move
@@ -70,6 +75,15 @@
           //message appropriately
         };
       }
+      // On load this calls the newGame method, because Angular Documentation advises against
+      // using ngInit to call a function on load to initialize values on scope
+      // Reference: https://docs.angularjs.org/api/ng/directive/ngInit
+      this.init = function(){
+        return this.newGame();
+      };
+
+      this.init();
+
     }
   ]);
 })();
